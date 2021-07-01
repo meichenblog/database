a740e29ada788584d12b3ae45836142ffac0d916
MeiChen <13316899740@163.com>
2021-07-01 11:20:30 +0800

    Update dbeaver.sh

--- DBTools/dbeaver.sh
+++ DBTools/dbeaver.sh
@@ -6,3 +6,6 @@ https://github.com/dbeaver/cloudbeaver
 sudo docker pull dbeaver/cloudbeaver:latest
 
 sudo docker run --name cloudbeaver --rm -ti -p 8080:8978 -v /var/cloudbeaver/workspace:/opt/cloudbeaver/workspace dbeaver/cloudbeaver:latest
+
+# 一款SQL检查审核工具(MySql)
+wget https://github.com/cookieY/Yearning/releases/download/v2.3.3/Yearning-2.3.3.2-linux-amd64.zip
